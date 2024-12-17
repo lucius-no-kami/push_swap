731fdcae4bfc4836d07698a4f2101b80cf632a7f
luluzuri <luluzuri@student.42.fr>
2024-12-17 10:12:32 +0100

    First push of the project

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	push_swap();
+	return (0);
+}
---
+++ src/push_swap.c
@@ -0,0 +1,7 @@
+#include "push_swap.h"
+
+int	push_swap(void)
+{
+	printf("This is the main logic of push_swap!\n");
+	return (0);
+}
