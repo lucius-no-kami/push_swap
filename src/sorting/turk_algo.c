0b11c7daa062eb82fad99745f0e8ff6e92da934e
luluzuri <luluzuri@student.42.fr>
2024-12-27 14:45:07 +0100

    Added turk_utils.c

--- src/sorting/turk_algo.c
+++ src/sorting/turk_algo.c
@@ -6,63 +6,11 @@
 /*   By: luluzuri <luluzuri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 12:57:36 by luluzuri          #+#    #+#             */
-/*   Updated: 2024/12/25 18:23:25 by luluzuri         ###   ########.fr       */
+/*   Updated: 2024/12/27 14:43:42 by luluzuri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-static void set_cost(t_stack *a, t_stack *b)
-{
-	
-}
-
-static void	set_target(t_stack *a, t_stack *b)
-{
-	t_stack	*current;
-	t_stack	*target;
-	long	match;
-
-	while (b)
-	{
-		current = a;
-		match = LONG_MAX;
-		while (current)
-		{
-			if (current->value > b->value && current->value < match)
-			{
-				b->target_node = current;
-				match = current->value;
-			}
-			current = current->next;
-		}
-		if (match == LONG_MAX)
-			b->target_node = get_smallest(a);
-		else
-			b->target_node = target;
-		b = b->next;
-	}
-}
-
-static void	set_mediane(t_stack **head)
-{
-	size_t	stack_len;
-	t_stack	*current;
-
-	stack_len = get_stack_len(head);
-	current = *head;
-	while (current)
-	{
-		if (stack_len > 0)
-		{
-			current->is_above_median = 1;
-			stack_len--;
-		}
-		else
-			current->is_above_median = 0;
-		current = current->next;
-	}
-}
 
 void	turk_algorithm(t_stack **a, t_stack **b)
 {
